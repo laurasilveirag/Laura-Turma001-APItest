04039a9807fa795408a321ba37c6794f7281e693
Laura Silveira Goncalves <laura.73821@alunosatc.edu.br>
2025-04-29 19:38:29 -0300

    dez testes

--- test/api_mercado.spec.ts
+++ test/api_mercado.spec.ts
@@ -131,6 +131,15 @@ describe('Testes API Mercado', () => {
       });
   });
 
+  it('Erro ao buscar mercado com ID inexistente', async () => {
+    const idInexistente = 999999;
+    await request
+      .spec()
+      .get(`${apiUrl}/mercado/${idInexistente}`)
+      .expectStatus(StatusCodes.NOT_FOUND)
+      .expectBodyContains('Mercado não encontrado');
+  });
+
   it('Atualizar mercado', async () => {
     await request
       .spec()
@@ -150,6 +159,31 @@ describe('Testes API Mercado', () => {
           cnpj: String
         }
       });
+  });
+
+  it('Erro ao atualizar mercado com CNPJ inválido', async () => {
+    await request
+      .spec()
+      .put(`${apiUrl}/mercado/${mercadoId}`)
+      .withJson({
+        nome: faker.company.name(),
+        endereco: faker.location.streetAddress(),
+        cnpj: faker.number.int({ min: 1000000000000, max: 9999999999999 })
+      })
+      .expectStatus(StatusCodes.BAD_REQUEST)
+      .expectBodyContains('CNPJ deve ter 14 dígitos');
+  });
+
+  it('Erro ao atualizar mercado sem nome', async () => {
+    await request
+      .spec()
+      .put(`${apiUrl}/mercado/${mercadoId}`)
+      .withJson({
+        endereco: faker.location.streetAddress(),
+        cnpj: faker.number.int({ min: 10000000000000, max: 99999999999999 })
+      })
+      .expectStatus(StatusCodes.BAD_REQUEST)
+      .expectBodyContains('Nome é obrigatório');
   });
 
   it('Listar todos mercados', async () => {
